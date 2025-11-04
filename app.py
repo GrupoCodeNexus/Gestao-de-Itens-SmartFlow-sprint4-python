a54f26b6b2d866e5e34a223dd8beed591d72e0aa
Kayque <kayquecarvalho020@gmail.com>
2025-11-03 22:32:57 -0300

    Refactor: Ajustando root do projeto app.py

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-from flask import Flask
+from flask import Flask, redirect
 from pathlib import Path
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="templates", static_folder="static")
 
-    # garante pastas
+    # Garante a existência das pastas
     Path("dados").mkdir(exist_ok=True)
     Path("dados/pacientes").mkdir(exist_ok=True)
     Path("dados/estoque").mkdir(exist_ok=True)
 
     # importa blueprints
-    from blueprints.pacientes.routes import pacientes_bp
+    from blueprints.paciente.routes import paciente_bp
     from blueprints.estoque.routes import estoque_bp
     from blueprints.token.routes import token_bp
     from blueprints.resumo.routes import resumo_bp
 
     # registra blueprints
-    app.register_blueprint(pacientes_bp, url_prefix="/pacientes")
+    app.register_blueprint(paciente_bp, url_prefix="/paciente")
     app.register_blueprint(estoque_bp, url_prefix="/estoque")
     app.register_blueprint(token_bp, url_prefix="/token")
     app.register_blueprint(resumo_bp, url_prefix="/resumo")
@@ -24,7 +24,7 @@ def create_app():
     # rota inicial -> seleção paciente
     @app.route("/")
     def index():
-        return "<script>location.href='/pacientes/selecionar'</script>"
+        return redirect("/paciente/selecionar")
 
     return app
 
